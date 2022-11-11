36c767aacd08a047b5be25b4a38051ba28f98a25
Jonas16-prog <117990131+Jonas16-prog@users.noreply.github.com>
2022-11-11 15:58:41 +0100

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -28,7 +28,7 @@ source_suffix = {
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'pydata_sphinx_theme'
-html_static_path = ['_static']
+html_static_path = ['_static_']
 html_title = "Jonas"
 
 html_theme_options = {
