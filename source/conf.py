b986b387faeaf4a7aa5648ce1e230168f0388806
Jonas16-prog <117990131+Jonas16-prog@users.noreply.github.com>
2022-11-11 15:54:32 +0100

    move req

--- source/conf.py
+++ source/conf.py
@@ -29,6 +29,7 @@ source_suffix = {
 
 html_theme = 'pydata_sphinx_theme'
 html_static_path = ['_static']
+html_title = "Jonas"
 
 html_theme_options = {
   "github_url": "https://github.com/vferat/",
---
+++ source/index.md
@@ -0,0 +1,11 @@
+# Jonas
+
+Bienvenue sur notre site web
+``` (toctree)
+:maxdepth: 2
+:hidden:
+about
+projects/index
+publications
+blog
+```
